e024900c79c80211b2d6293eee86609beed2ced3
AllTeach <33581448+AllTeach@users.noreply.github.com>
2021-10-29 22:43:55 +0300

    added taking pictures

---
+++ app/src/main/java/com/example/resultsapi/GenericResultActivity.java
@@ -0,0 +1,14 @@
+package com.example.resultsapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenericResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generic_result);
+    }
+}
